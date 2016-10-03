e1a2b23a3274cd6103c95fa804066af644f609d0
Arpith K <arpith.k@csa.iisc.ernet.in>
2016-10-03 17:56:35 +0530

    Minified!

--- scripts/pithy.js
+++ scripts/pithy.js
@@ -1,18 +1,1 @@
-$(function(){
-    $("#gotop").click(function(){
-        jQuery("html,body").animate({
-            scrollTop:0
-        }, 500);
-    });
-    $(window).load(function() {
-		$('#gotop').hide();
-    })
-    
-    $(window).scroll(function() {
-        if ( $(this).scrollTop() > 300){
-            $('#gotop').fadeIn("fast");
-        } else {
-            $('#gotop').stop().fadeOut("fast");
-        }
-    });
-});
+$(function(){$("#gotop").click(function(){jQuery("html,body").animate({scrollTop:0},500)}),$(window).load(function(){$("#gotop").hide()}),$(window).scroll(function(){$(this).scrollTop()>300?$("#gotop").fadeIn("fast"):$("#gotop").stop().fadeOut("fast")})});
