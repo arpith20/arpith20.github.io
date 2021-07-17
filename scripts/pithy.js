7d766892f4bdb0b2f116f20e9c937fa288e9c7c3
Arpith K <arpith@live.com>
2021-07-17 11:51:05 +0530

    Fix minify issues

--- scripts/pithy.js
+++ scripts/pithy.js
@@ -1,18 +1,1 @@
-$(function(){
-    $("#gotop").click(function(){
-        jQuery("html,body").animate({
-            scrollTop:0
-        }, 500);
-    });
-    $(window).load(function() {
-		$('#gotop').hide();
-    })
-    
-    $(window).scroll(function() {
-        if ( $(this).scrollTop() > 300){
-            $('#gotop').fadeIn("fast");
-        } else {
-            $('#gotop').stop().fadeOut("fast");
-        }
-    });
-});
+$(function(){$("#gotop").click(function(){jQuery("html,body").animate({scrollTop:0},500)}),$(window).load(function(){$("#gotop").hide()}),$(window).scroll(function(){300<$(this).scrollTop()?$("#gotop").fadeIn("fast"):$("#gotop").stop().fadeOut("fast")})});
